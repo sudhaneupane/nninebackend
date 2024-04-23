1a987ed8373537c4c502eba692e2c1b80f0f616e
Sudha <sudaneupane.11@gmail.com>
2024-04-23 20:55:35 +0545

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import express from "express";
+// import mongoose from "mongoose";
+
+// const app=express()
+
+
+// app.listen(port,`Server listening at port http://localhost${process.env.PORT}`)

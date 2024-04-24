8b4a6ee1019f7fc1cc06b020b7531152ff9443b2
Mingtindu <mingmartu@gmail.com>
2024-04-24 07:50:30 +0545

    feat: index file configured

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@
 
 
 // app.listen(port,`Server listening at port http://localhost${process.env.PORT}`)
+import connectDB from './db/index.js'
+import {app} from './app.js';
+import dotenv from 'dotenv'
+dotenv.config({
+    path: './.env'
+})
+
+connectDB().then(()=>{
+    app.listen(process.env.port || 8000,()=>{
+        console.log('App is running on port 8000');
+    }
+
+    )
+}).catch((err)=>{
+    console.log(`Mongodb connection failed err::${err}`);
+
+})

a5b54c14ddbe5034962ff85121952f273e37a789
Sudha <sudaneupane.11@gmail.com>
2024-04-26 10:45:28 +0545

    async handler added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+
+const asyncHandler=(func)=>async(req,res,next)=>{
+    try {
+        await func(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            msg:error.message,
+            success:false
+        })
+    }
+}

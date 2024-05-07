cc4189bbe9f707ac805e2b99d38ece24cc25f718
Mingtindu <mingmartu@gmail.com>
2024-05-07 12:20:14 +0545

    testing

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ app.use(cors({
     credentials:true
 }))
 app.use(express.json({limit:"20kb"}))//accept the json from frontend
-app.use(express.static("public"))
-
 // Serve static files from the "public" directory
 app.use(express.static("public"));
 //router import :
@@ -15,8 +13,6 @@ import enrollUser from './router/enroll.router.js'
 
 //router declaration ::
 app.use('/api/v1/courses',enrollUser)
-
-
 // routes declaration
 app.use('/',(req,res)=>{
     res.send("this is main hai")

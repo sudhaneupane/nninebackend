28b04c6578129f9a5035c694360a4e2128018655
Mingtindu <mingmartu@gmail.com>
2024-05-09 11:46:21 +0545

    user login code reviewed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcrypt'
 const checkUser = async(req,res)=>{
     //retrieve data from frontend::
     //check if empty
@@ -12,7 +13,16 @@ const checkUser = async(req,res)=>{
         res.status(401).json({error:"All the fields are required"})
     }
     const checkUser = await User.findOne({email});
-    console.log(checkUser)
+    const hashPassword = checkUser.password;
+    const match = await bcrypt.compare(password,hashPassword)
+    if(match){
+        return res.status(201).json({message:"success"})
+    }
+    res.status(402).json(
+        {
+            error:"password does not match"
+        }
+    )
 
 }
 const addUser = async(req,res)=>{

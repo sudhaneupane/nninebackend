b0c2fe4f584892596b944cec05e17be7e96ab53b
Sudha <sudaneupane.11@gmail.com>
2024-05-22 16:58:14 +0545

    few fixes

--- src/controllers/otp.controller.js
+++ src/controllers/otp.controller.js
@@ -1,9 +1,7 @@
 import OTP from "../models/otp.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { sendMail } from "../services/sendMail.js";
-import { generateOtp } from "../services/generateOtp.js";
 
-const verifyOtp = async (req, res) => {
+const verifyOtp = asyncHandler(async (req, res) => {
   let { email, otp } = req.body;
   otp = Number(otp);
   if (!email) {
@@ -14,18 +12,20 @@ const verifyOtp = async (req, res) => {
   }
   try {
     const savedOtp = await OTP.findOne({ email });
-    const checkOtp = savedOtp.otp === otp;
-    if (!checkOtp) {
-      res.status(400).json("Invalid email or OTP");
+
+    if (!savedOtp || savedOtp.otp !== otp) {
+      return res.status(400).json({ error: "Invalid email or OTP" });
     }
-    const deleteOtp = await savedOtp.deleteOne({ otp });
-    if (deleteOtp) {
-      res.status(200).json("Ramro xa otp");
-    }
+
+    await savedOtp.deleteOne();
+
+    return res.status(200).json({ message: "Successful" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json("An error occured while verifying the code");
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while verifying the code" });
   }
-};
+});
 
 export { verifyOtp };

ad392a1a7b8df92a901ae058b12ceac0f33aeda3
Mingtindu <mingmartu@gmail.com>
2024-05-22 23:31:17 +0545

    feat:get student by courseId

--- src/controllers/student.contoller.js
+++ src/controllers/student.contoller.js
@@ -35,7 +35,22 @@ const studentInfo = asyncHandler(async (req, res) => {
     res.status(500).json("error occured while saving data");
   }
 });
-
+const getStudentByCourse = async(req,res)=>{
+  try {
+    const courseId = req.query.courseId;
+    if(!courseId){
+      return res.status(400).json({message:"Missing courseId"})
+    }
+    const students = await Student.find({courseId})
+    if (!students || !students.length) {
+      return res.status(404).json({ message: "No students found for this course." });
+    }
+    res.status(200).json(students);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Error retrieving student details." });
+  }
+}
 const getStudentDetail = asyncHandler(async (req, res) => {
   try {
     const studentDetail = await Student.find({});
@@ -45,5 +60,5 @@ const getStudentDetail = asyncHandler(async (req, res) => {
   }
 });
 
-export { studentInfo, getStudentDetail };
+export { studentInfo, getStudentDetail,getStudentByCourse };
 
--- src/router/student.router.js
+++ src/router/student.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {getStudentDetail, studentInfo} from "../controllers/student.contoller.js";
+import {getStudentDetail, studentInfo,getStudentByCourse} from "../controllers/student.contoller.js";
 const router=Router()
 
 router.route('/save').post(studentInfo)
 router.route('/logs').get(getStudentDetail)
+router.route('/by-course').get(getStudentByCourse)
 
 export default router

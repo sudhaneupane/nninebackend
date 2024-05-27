aebbcc03e1dfef82b0b0a8b2a412660d10474173
Mingtindu <mingmartu@gmail.com>
2024-05-27 21:24:57 +0545

    Refactor:Postgress connection in backend

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,25 @@
-import mongoose from "mongoose";
-import DB_NAME from "../constant.js";
-const connectDB = async()=>{
-try{
-    const connection =await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-    console.log(`Mongo DB Connected : HOST:: ${connection.connection.host}`);
-}catch(err){
-    console.log(`Mongo connection failed err:: ${err}`);
-    process.exit(1);
-}
-}
+import pkg from 'pg';
+const { Client } = pkg;
+const connectDB = async () => {
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL database");
+
+    const res = await client.query('SELECT NOW()');
+    console.log('Query result:', res.rows);
+  } catch (err) {
+    console.error('Connection or query error', err.stack);
+  } finally {
+    await client.end();
+  }
+};
 
 export default connectDB
